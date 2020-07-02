99f48d2220700f16151617f8e49e21e297757f68
Tuan <=>
2020-07-02 11:43:00 +0700

    tuan update

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\About;
+use Illuminate\Http\Request;
+use App\Models\Abouts;
+
+class AboutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $abouts = Abouts::paginate(8);
+        return response()->json([
+            'abouts'=>$abouts
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        Abouts::create($data);
+        return response()->json([
+            'message'=>'Tạo mới thành công'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function show(About $about)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $about = Abouts::find($id);
+        return response()->json([
+            'data'=>$about
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $about = Abouts::find($id);
+        $data = $request->all();
+        $about->update($data);
+        return response()->json([
+            'messsage'=>'Update thành công'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $about = Abouts::find($id);
+        $about->delete();
+        return response()->json([
+            'message'=>'Xóa thành công'
+        ]);
+    }
+}
---
+++ app/Models/Abouts.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Abouts extends Model
+{
+    protected $table = "abouts";
+    protected $fillable = [
+        'name','content'
+    ];
+}
